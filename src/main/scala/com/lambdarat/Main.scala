3595441a5fdc0586d80c5e12216dd9e3743570fc
Roberto Serrano <r.semarcos@gmail.com>
2019-08-08 01:47:25 +0200

    Complete window example extracting to its own service

--- src/main/scala/com/lambdarat/Main.scala
+++ src/main/scala/com/lambdarat/Main.scala
@@ -1,11 +1,10 @@
 package com.lambdarat
 
-import com.lambdarat.Main.GlfwError.{GlfwInitError, GlfwShowWindowError, GlfwWindowCreationError}
+import com.lambdarat.ui.Window.WindowId
+import com.lambdarat.ui.{Window, WindowLive, zwindow}
 import org.lwjgl.glfw.GLFW._
-import org.lwjgl.glfw.GLFWErrorCallback
-import org.lwjgl.system.MemoryUtil._
-import zio.console._
-import zio.{App, IO, ZIO}
+import zio.clock.Clock
+import zio.{App, Schedule, ZIO}
 
 object Main extends App {
 
@@ -13,33 +12,33 @@ object Main extends App {
   object GlfwError {
     final case object GlfwInitError           extends GlfwError("GLFW Initialization error")
     final case object GlfwWindowCreationError extends GlfwError("GLFW Window creation error")
-    final case class GlfwShowWindowError(window: Long)
-        extends GlfwError(s"GLFW Error showing window ${window}")
   }
 
-  override def run(args: List[String]): ZIO[Main.Environment, Nothing, Int] =
+  override def run(args: List[String]): ZIO[Main.Environment, Nothing, Int] = {
+    main
+      .provideSome[Environment](
+        base =>
+          new Window with Clock {
+            override def window: Window.Service    = WindowLive.window
+            override val clock: Clock.Service[Any] = base.clock
+          }
+      )
+      .fold(_ => 1, identity)
+  }
+
+  private def loopUntilWindowClose(windowId: WindowId): ZIO[Clock, Throwable, Boolean] =
+    ZIO
+      .effect(glfwWindowShouldClose(windowId))
+      .tap(shouldClose => ZIO.when(!shouldClose)(ZIO.effect(glfwPollEvents())))
+      .repeat(Schedule.doUntil[Boolean](identity))
+
+  def main: ZIO[Window with Clock, Throwable, Int] =
     for {
-      _ <- initialize
-        .catchAll(err => putStrLn(s"[SUNK] Failed with ${err.msg}") *> ZIO.succeed(1))
-        .fork
-      _ <- ZIO.never
+      _   <- zwindow.initialize
+      wid <- zwindow.createWindow(Window.WindowWidth(300), Window.WindowHeight(300), "Hello world!")
+      _   <- zwindow.showWindow(wid)
+      _   <- loopUntilWindowClose(wid)
+      _   <- zwindow.destroyWindow(wid)
     } yield 0
-
-  val setupErrorCallback: ZIO[Any, GlfwError.GlfwInitError.type, GLFWErrorCallback] =
-    ZIO.effect(GLFWErrorCallback.createPrint(System.err).set()).mapError(_ => GlfwInitError)
-  val initializeGlfw: ZIO[Any, GlfwError.GlfwInitError.type, Boolean] =
-    ZIO.effectTotal(glfwInit).filterOrFail(identity)(GlfwInitError)
-  val createWindow: ZIO[Any, GlfwError.GlfwWindowCreationError.type, Long] = ZIO
-    .effect(glfwCreateWindow(300, 300, "Hello World!", NULL, NULL))
-    .mapError(_ => GlfwWindowCreationError)
-  def showWindow(window: Long): ZIO[Any, GlfwShowWindowError, Unit] =
-    ZIO.effect(glfwShowWindow(window)).mapError(_ => GlfwShowWindowError(window))
-
-  val initialize: IO[GlfwError, Int] = for {
-    _      <- setupErrorCallback
-    _      <- initializeGlfw
-    window <- createWindow
-    _      <- showWindow(window)
-  } yield 0
 
 }
